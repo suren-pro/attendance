d3716b02b6f5bf46844bab4e3943deed8de4690c
Suren <surenbarseghyan13@gmail.com>
2019-12-08 20:55:10 +0400

    sfwq

---
+++ Online Attendance/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_Attendance
+{
+
+     
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = Console.ReadLine();
+            Action action;
+
+            switch (name)
+            {
+                case "Steven":
+                case "Jack":
+                 case "Mathew":
+
+                    action = Alarm;
+
+                    break;
+                default:
+                    action = ()=> Console.WriteLine($"Welcome {name} ");
+
+                    break;
+
+            }
+
+            action();
+            Console.ReadKey();
+        }
+
+        static void Alarm()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("You are banned.Logging out!!!");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Beep();
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+        }
+    }
+}
